bbeafcf0101cb00757df4144cc039f3f8a37b3a5
Patrick Brennan <pbrennanmusic@gmail.com>
2021-12-22 16:20:39 -0500

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -15,15 +15,18 @@ class CreateTodo extends Component {
   handleSubmit = event => {
     event.preventDefault();
     this.props.addTodo(this.state)
+    this.setState({
+      text: ""
+    })
   }
-  
+
   render() {
     return(
       <div>
         <form onSubmit={this.handleSubmit}>
           <input
             type="text"
-            placeholder="add todo" 
+            placeholder="add todo"
             value={this.state.text}
             onChange={this.handleChange}
           />
